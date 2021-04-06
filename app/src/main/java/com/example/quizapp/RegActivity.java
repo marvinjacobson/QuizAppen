87db29fd23db36151be16d84caec618c16b3c57a
DanielBredberg <56878344+DanielBredberg@users.noreply.github.com>
2021-04-06 17:44:49 +0200

    små ändring

--- app/src/main/java/com/example/quizapp/RegActivity.java
+++ app/src/main/java/com/example/quizapp/RegActivity.java
@@ -63,47 +63,7 @@ public class RegActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    private void showSignUpDialog() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(RegActivity.this);
-        alertDialog.setTitle("Sign up");
-        alertDialog.setMessage("Please fill full information");
 
-        alertDialog.setIcon(R.drawable.ic_baseline_person_24);
-
-        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                User user = new User(edtNewUser.getText().toString(), edtNewPassword.getText().toString(), edtNewEmail.getText().toString());
-
-                users.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.child(user.getUserName()).exists())
-                            Toast.makeText(RegActivity.this, "User exists", Toast.LENGTH_SHORT).show();
-                        else {
-                            users.child(user.getUserName())
-                                    .setValue(user);
-                            Toast.makeText(RegActivity.this, "User registration success", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-                dialog.dismiss();
-            }
-        });
-        alertDialog.show();
-    }
     public void createAccount(){
             User user = new User(edtNewUser.getText().toString(), edtNewPassword.getText().toString(), edtNewEmail.getText().toString());
 
@@ -116,6 +76,7 @@ public class RegActivity extends AppCompatActivity {
                         users.child(user.getUserName())
                                 .setValue(user);
                         Toast.makeText(RegActivity.this, "User registration success", Toast.LENGTH_SHORT).show();
+                        openNewActivity();
                     }
                 }
 
