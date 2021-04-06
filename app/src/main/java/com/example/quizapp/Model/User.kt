3c0f5b66cc3757cc43d9e19350bb1a551b3b9576
Ragnar Åhlin <ahlinragnar@gmail.com>
2021-04-06 11:36:04 +0200

    User.java gjordes om till User.kt

--- app/src/main/java/com/example/quizapp/Model/User.kt
+++ app/src/main/java/com/example/quizapp/Model/User.kt
@@ -1,40 +1,14 @@
-package com.example.quizapp.Model;
+package com.example.quizapp.Model
 
-public class User {
-    private String userName;
-    private String password;
-    private String email;
+class User {
+    var userName: String? = null
+    var password: String? = null
+    var email: String? = null
 
-    public User() {
-    }
-
-    public User(String userName, String password, String email) {
-        this.userName = userName;
-        this.password = password;
-        this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    constructor() {}
+    constructor(userName: String?, password: String?, email: String?) {
+        this.userName = userName
+        this.password = password
+        this.email = email
     }
 }
