5f145eb6da89a3c96c80fa5ac22eb305ccb7611b
DanielBredberg <56878344+DanielBredberg@users.noreply.github.com>
2021-04-14 11:13:50 +0200

    Bättre reg

--- app/src/main/java/com/example/quizapp/RegActivity.java
+++ app/src/main/java/com/example/quizapp/RegActivity.java
@@ -1,11 +1,13 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.quizapp.Model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,10 +31,9 @@ import java.util.Objects;
 
 public class RegActivity extends AppCompatActivity {
     EditText edtNewUser, edtNewPassword, edtNewEmail;
-
     TextView tvLoggin;
     Button btn_sign_up;
-
+    private FirebaseAuth auth;
     FirebaseDatabase database;
     DatabaseReference users;
     @Override
@@ -42,7 +47,7 @@ public class RegActivity extends AppCompatActivity {
         edtNewUser = (EditText)findViewById(R.id.edtUser);
         edtNewEmail = (EditText)findViewById(R.id.edtNewEmail);
         edtNewPassword = (EditText)findViewById(R.id.edtPassword);
-
+        auth = FirebaseAuth.getInstance();
         tvLoggin = (TextView)findViewById(R.id.tv_loggin);
         btn_sign_up = (Button)findViewById(R.id.btn_sign_up);
 
@@ -55,9 +60,37 @@ public class RegActivity extends AppCompatActivity {
         btn_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               createAccount();
+               String email = edtNewEmail.getText().toString();
+               String pw = edtNewPassword.getText().toString();
+
+               if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)){
+                   Toast.makeText(RegActivity.this, "Kom ihåg att fylla alla fält", Toast.LENGTH_SHORT).show();
+            }
+               else if(pw.length() < 6){
+                   Toast.makeText(RegActivity.this, "Lösenordet är inte starkt nog!", Toast.LENGTH_SHORT).show();
+               }
+               else{
+                   registerUser(email, pw);
+
+               }
             }
         });
+    }
+
+    private void registerUser(String email, String pw) {
+        auth.createUserWithEmailAndPassword(email,pw).addOnCompleteListener(RegActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(RegActivity.this, "Konto regsisterat", Toast.LENGTH_SHORT).show();
+                    openMain();
+                }
+                else{
+                    Toast.makeText(RegActivity.this, "Konto registrering misslyckad", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     //Gå till main view
@@ -65,29 +98,5 @@ public class RegActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-    //Skickar username, lösenord och mail till firebase.
-    public void createAccount(){
-        try{
-            User user = new User(edtNewUser.getText().toString(), edtNewPassword.getText().toString(), edtNewEmail.getText().toString());
-            users.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.child(Objects.requireNonNull(user.getUserName())).exists())
-                        Toast.makeText(RegActivity.this, "User exists", Toast.LENGTH_SHORT).show();
-                    else {
-                        users.child(user.getUserName())
-                                .setValue(user);
-                        Toast.makeText(RegActivity.this, "User registration success", Toast.LENGTH_SHORT).show();
-                        openMain();
-                    }
-                }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
-            });
-        }catch (Exception e) {
-            System.out.println(e);
-        }
-    }
 }
