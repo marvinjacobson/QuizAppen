9f16219fe100f930815face758f02bc6eaf7710c
marvinjac <56878406+marvinjac@users.noreply.github.com>
2021-04-22 10:09:19 +0200

    Kategorier version 1

--- app/src/main/java/com/example/quizapp/Categories.java
+++ app/src/main/java/com/example/quizapp/Categories.java
@@ -1,10 +1,29 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.quizapp.Interface.ItemClickListener;
+import com.example.quizapp.Model.Category;
 import com.example.quizapp.R;
+import com.example.quizapp.ViewHolder.CategoryViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Categories extends AppCompatActivity {
 
@@ -12,5 +31,13 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new CategoryFragment()).commit();
+
+        }
+
     }
+
+
 }
