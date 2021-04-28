ce46f12d490897f9c6b87ca54e7f855227bee86b
marvinjac <56878406+marvinjac@users.noreply.github.com>
2021-04-28 10:22:30 +0200

    Tagit bort onödigt ang Kategorier

--- app/src/main/java/com/example/quizapp/Categories.java
+++ app/src/main/java/com/example/quizapp/Categories.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,12 +16,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.quizapp.Interface.ItemClickListener;
 import com.example.quizapp.Model.Category;
 import com.example.quizapp.R;
 import com.example.quizapp.ViewHolder.CategoryViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,19 +34,55 @@ import java.util.ArrayList;
 
 public class Categories extends AppCompatActivity {
 
+    RecyclerView listCategory;
+    FirebaseDatabase database;
+    DatabaseReference categories;
+    RecyclerView.LayoutManager layoutManager;
+    FirebaseRecyclerAdapter<Category, CategoryViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
-        if (savedInstanceState == null) {
+        /*if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(android.R.id.content, new CategoryFragment()).commit();
 
-        }
+        }*/
+        database = FirebaseDatabase.getInstance();
+        categories = database.getReference("Category");
+        listCategory = (RecyclerView) findViewById(R.id.listOfCat);
+        listCategory.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(Categories.this);
+        listCategory.setLayoutManager(layoutManager);
 
+        loadCategories();
 
     }
+
+    private void loadCategories() {
+        adapter = new FirebaseRecyclerAdapter<Category, CategoryViewHolder>(
+                Category.class,
+                R.layout.category_layout,
+                CategoryViewHolder.class,
+                categories
+        ) {
+            @Override
+            protected void populateViewHolder(CategoryViewHolder categoryViewHolder, Category category, int i) {
+                categoryViewHolder.category_name.setText(category.getName());
+
+                categoryViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
+            }
+        };
+        adapter.notifyDataSetChanged();
+        listCategory.setAdapter(adapter);
+    }
+
 
 
 }
--- app/src/main/java/com/example/quizapp/ViewHolder/CategoryViewHolder.java
+++ app/src/main/java/com/example/quizapp/ViewHolder/CategoryViewHolder.java
@@ -2,6 +2,7 @@ package com.example.quizapp.ViewHolder;
 
 import android.content.ClipData;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,14 +12,15 @@ import com.example.quizapp.Interface.ItemClickListener;
 import com.example.quizapp.R;
 
 public class CategoryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-    public TextView category_name;
+    /*public TextView category_name;*/
+    public Button category_name;
 
     private ItemClickListener itemClickListener;
 
     public CategoryViewHolder(@NonNull View itemView) {
         super(itemView);
-        category_name = (TextView)itemView.findViewById(R.id.categoryName);
+        /*category_name = (TextView)itemView.findViewById(R.id.categoryName);*/
+        category_name = (Button)itemView.findViewById(R.id.categoryName);
 
         itemView.setOnClickListener(this);
     }
