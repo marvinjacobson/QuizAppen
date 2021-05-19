5c0ae3d264f42a331292f0ffac5f3a35092e3e7e
marvinjac <56878406+marvinjac@users.noreply.github.com>
2021-05-19 11:36:02 +0200

    Hämta favorit kategori

--- app/src/main/java/com/example/quizapp/Home.java
+++ app/src/main/java/com/example/quizapp/Home.java
@@ -2,27 +2,45 @@ package com.example.quizapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quizapp.Common.Common;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
 
+    DatabaseReference mDatabase;
+
     private Button btnSettings, btn_createQuiz, btnCategories, btnFriends, btn_fave, btn_day;
+
+    private String favCat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         btnSettings = (Button) findViewById(R.id.btn_gotoSettings);
         btnFriends = (Button) findViewById(R.id.btn_gotofriends);
         btn_createQuiz = (Button) findViewById(R.id.btn_createQuiz);
         btnCategories = (Button) findViewById(R.id.btn_categories);
         btn_fave = (Button) findViewById(R.id.btn_fave);
         btn_day = (Button)findViewById(R.id.btn_quizOfDay);
+        getFav();
 
         //Öppnar Settings
         btnSettings.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +87,22 @@ public class Home extends AppCompatActivity {
         });
 
 
+    }
+
+    public void getFav(){
+        String currentUser = FirebaseAuth.getInstance().getUid();
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("Users").child(currentUser).child("favCategory");
+        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+               favCat = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void openPlay() {
